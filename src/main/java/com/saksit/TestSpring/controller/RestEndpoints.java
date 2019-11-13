411897cd407b2d7cccef2d65e5d4dab74046d850
fahlunsaksit <saksit.ban@gmail.com>
2019-11-13 16:58:19 +0700

    Understand the properties

--- src/main/java/com/saksit/TestSpring/controller/RestEndpoints.java
+++ src/main/java/com/saksit/TestSpring/controller/RestEndpoints.java
@@ -1,11 +1,26 @@
 package com.saksit.TestSpring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.StreamingHttpOutputMessage;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestEndpoints {
+
+    /*data comming from application.properties*/
+    @Value("${default.course.name}")
+    private String cName;
+
+    @Value("${default.corse.chapterCount}")
+    private int chaptersCount;
+    /*data comming from application.properties*/
+
+    @RequestMapping("/defaultCourse")
+    public Course getDefaultCourse(@RequestParam(value = "name", defaultValue = "saksit", required = false) String name,
+                              @RequestParam(value = "chapterCount", defaultValue = "2", required = false) int chapterCount
+    ) {
+        return new Course(cName, chaptersCount);
+    }
 
     @RequestMapping("/course")
     public Course getEndpoint(@RequestParam(value = "name", defaultValue = "saksit", required = false) String name,
@@ -13,6 +28,18 @@ public class RestEndpoints {
     ) {
         return new Course(name, chapterCount);
     }
+
+    @RequestMapping("/")
+    public String getEndpoint() {
+        return "WELCOME MY WEB SPRING";
+    }
+
+    @RequestMapping(method = RequestMethod.POST , value = "/register/course")
+    public String saveCourse(@RequestBody Course course){
+        return "Your code named "+ course.getName() + " with " + course.getChapterCount() + " chapters saved successfuly";
+    }
+
+
 
 
 }
