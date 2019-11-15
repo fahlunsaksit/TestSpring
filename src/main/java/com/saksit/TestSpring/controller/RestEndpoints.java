52cb601dc9231e2c67376808feeaa087b92ab5a8
fahlunsaksit <saksit.ban@gmail.com>
2019-11-15 14:17:05 +0700

    @ConfigurationProperties getHierarchical Properties

--- src/main/java/com/saksit/TestSpring/controller/RestEndpoints.java
+++ src/main/java/com/saksit/TestSpring/controller/RestEndpoints.java
@@ -1,8 +1,11 @@
 package com.saksit.TestSpring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class RestEndpoints {
@@ -12,18 +15,35 @@ public class RestEndpoints {
      * */
 
     /*data comming from application.properties*/
-    @Value("${default.course.name}")
+    @Value("${course.name}")
     private String cName;
 
-    @Value("${default.corse.chapterCount}")
+    @Value("${course.chapterCount}")
     private int chaptersCount;
     /*data comming from application.properties*/
+
+    @Autowired
+    private CourseConfiguration courseConfiguration;
 
     @RequestMapping("/defaultCourse")
     public Course getDefaultCourse(@RequestParam(value = "name", defaultValue = "saksit", required = false) String name,
                                    @RequestParam(value = "chapterCount", defaultValue = "2", required = false) int chapterCount
     ) {
         return new Course(cName, chaptersCount);
+    }
+
+
+    @RequestMapping("/getHierarchical")
+    public HashMap<String,Object> getConfigAnnotateProperties() {
+        HashMap<String,Object> map = new HashMap<String,Object>();
+
+        map.put("name", courseConfiguration.getName());
+        map.put("chapterCount", courseConfiguration.getChapterCount());
+        map.put("rating", courseConfiguration.getRating());
+        map.put("author", courseConfiguration.getAuthor());
+
+
+        return map;
     }
 
     @RequestMapping("/course")
